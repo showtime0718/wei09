cde9a5895d077fe81eb057ce85b8385091f39897
apple <apple@applede-MacBook-Pro.local>
2017-06-16 09:32:57 +0800

    20170616

---
+++ wei09/Myclass.swift
@@ -0,0 +1,40 @@
+//
+//  Myclass.swift
+//  wei09
+//
+//  Created by apple on 2017/6/16.
+//  Copyright © 2017年 swift006. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01{}
+class MyClass02{
+    var x:Int
+    init(x:Int){self.x = x}
+
+}
+
+class MyClass03{
+    var x = 123
+    init(){}   //<-可有可無 已經初始化
+}
+
+class MyClass04{
+    var x:Int?
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
